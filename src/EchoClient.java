e0ed12023450bfa976c836fd17d49b917064cdaa
Dontae Malone <dontae.malone2@gmail.com>
2015-02-13 13:26:49 -0500

    Started EchoClient logic

--- src/EchoClient.java
+++ src/EchoClient.java
@@ -7,7 +7,7 @@
 /**---------------------------------------------------------------------------------------------
  | Classification: UNCLASSIFIED
  |
- | Abstract: this application is still appart of the chattyKathy application as it will also have
+ | Abstract: this application is still apart of the chattyKathy application as it will also have
  |echo functionality
  |
  \---------------------------------------------------------------------------------------------*/
@@ -17,128 +17,43 @@
 
  \---------------------------------------------------------------------------------------------**/
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
-import java.nio.channels.SocketChannel;
-import java.util.Iterator;
+import java.util.*;
 
-public class EchoClient {
+public class EchoClient{
 
     public static void main(String args[]) throws Exception{
-        String string1 = "Let the bodies hit the floor";
-        //String string2 = "Second message";
-        SocketTest test1 = new SocketTest(string1);
-        Thread thread = new Thread(test1);
+        String input;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please type a message to echo");
+        input = scanner.nextLine();
+
+        System.out.println("You typed: " + input);
+
+        Messenger message = new Messenger(input);
+
+        Thread thread = new Thread(message);
         thread.start();
-        //thread2.start();
     }
 
-    static class SocketTest implements Runnable {
-
+    static class Messenger implements Runnable{
         private String message = "";
         private Selector selector;
 
-
-        public SocketTest(String message){
+        public Messenger(String message){
             this.message = message;
         }
 
-        @Override
-        public void run() {
-            SocketChannel channel;
-            try {
-                selector = Selector.open();
-                channel = SocketChannel.open();
-                channel.configureBlocking(false);
+        public void run(){
 
-                channel.register(selector, SelectionKey.OP_CONNECT);
-                channel.connect(new InetSocketAddress("127.0.0.1", 10000));
-
-                while (!Thread.interrupted()){
-
-                    selector.select(1000);
-
-                    Iterator<SelectionKey> keys = selector.selectedKeys().iterator();
-
-                    while (keys.hasNext()) {
-                        SelectionKey key = keys.next();
-                        keys.remove();
-
-                        if (!key.isValid()) continue;
-
-                        if (key.isConnectable()) {
-                            System.out.println("I am connected to the server");
-                            connect(key);
-                        }
-                        if (key.isWritable()) {
-                            write(key);
-                        }
-                        if (key.isReadable()) {
-                            read(key);
-                        }
-                    }
-                }
-            } catch (IOException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            } finally {
-                close();
-            }
         }
 
         private void close(){
-            try {
-                selector.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+
         }
 
-        private void read (SelectionKey key) throws IOException {
-            SocketChannel channel = (SocketChannel) key.channel();
-            ByteBuffer readBuffer = ByteBuffer.allocate(1000);
-            readBuffer.clear();
-            int length;
-            try{
-                length = channel.read(readBuffer);
-            } catch (IOException e){
-                System.out.println("Reading problem, closing connection");
-                key.cancel();
-                channel.close();
-                return;
-            }
-            if (length == -1){
-                System.out.println("Nothing was read from server");
-                channel.close();
-                key.cancel();
-                return;
-            }
-            readBuffer.flip();
-            byte[] buff = new byte[1024];
-            readBuffer.get(buff, 0, length);
-            System.out.println("Server said: "+new String(buff));
-        }
-
-        private void write(SelectionKey key) throws IOException {
-            SocketChannel channel = (SocketChannel) key.channel();
-            channel.write(ByteBuffer.wrap(message.getBytes()));
-
-            // lets get ready to read.
-            key.interestOps(SelectionKey.OP_READ);
-        }
-
-        private void connect(SelectionKey key) throws IOException {
-            SocketChannel channel = (SocketChannel) key.channel();
-            if (channel.isConnectionPending()){
-                channel.finishConnect();
-            }
-            channel.configureBlocking(false);
-            channel.register(selector, SelectionKey.OP_WRITE);
-        }
-
+        private void read
     }
+
 }
